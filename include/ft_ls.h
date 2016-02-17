80a61f3b663cb9ef73d76b0e50b146f7045cf500
Jean-baptiste GUTHERTZ <jguthert@e3r3p7.42.fr>
2016-02-17 14:37:07 +0100

    ft_ls list flag added

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/08 17:56:10 by jguthert          #+#    #+#             */
-/*   Updated: 2016/02/08 18:48:44 by jguthert         ###   ########.fr       */
+/*   Updated: 2016/02/17 12:49:52 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 # define FT_LS_H
 
 /*
-** TODO: Remove stdio.h
+** TODO:
 */
 
 # include <stdio.h> //
 
 # include "libft.h"
 
-# define ERROR ft_error(__FILE__, __FUNCTION__, __LINE__)
-# define ERRORNO ft_errorno(__FILE__, __FUNCTION__, __LINE__)
-
 /*
 **Name: Main
 **File: main.c
 **Desc: main
 */
+
+
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,44 +6,61 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/08 18:00:58 by jguthert          #+#    #+#             */
-/*   Updated: 2016/02/16 18:37:57 by jguthert         ###   ########.fr       */
+/*   Updated: 2016/02/17 14:22:13 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int list_flag(int argc, char **argv)
+static int	is_in(char c, char *str)
 {
-	char const base[] = "ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1";
-	char		*tmp;
-	int			i;
-
-	i = 1;
-	while (i < argv)
+	while (*str)
 	{
-		tmp = argv[i];
-		if (*tmp != '-')
-		{
-			if (i == 1)
-				return (0);
-			else
-				return (1);
-		}
-		tmp++;
-		while (*tmp)
-		{
-			(*tmp != base[i++]);
-				tmp++;
-		}
-		argc--;
+		if (*str == c)
+			return (1);
+		str++;
 	}
 	return (0);
 }
 
 int list_flag(int argc, char **argv)
 {
-	char const base[] = "ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1";
-	if ()
+	char const base[] = "Ralrt";
+	char		*tmp;
+	int			i;
+
+	i = 1;
+	while (i < argc)
+	{
+		tmp = argv[i];
+		if (*tmp != '-')
+			return (0);
+		tmp++;
+		while (*tmp)
+		{
+			printf("option %c\n", *tmp);
+			if (is_in(*tmp, base) == 1)
+			{
+				printf("stack %c\n", *tmp);
+				//tab[j] = 1;
+			}
+			else
+			{
+				ft_putstr("ls: illegal option -- ");
+				ft_putchar_fd(*tmp, 2);
+				ft_putchar('\n');
+				return (1);
+			}
+			tmp++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int list_files(int argc, char **argv)
+{
+	printf("GOOD\n");
 	return (0);
 }
 
