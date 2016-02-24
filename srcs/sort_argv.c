dd4e058ee845f0d797af6bc6fa57ebb1a98c8892
Jean-baptiste GUTHERTZ <jguthert@e3r4p8.42.fr>
2016-02-25 00:00:39 +0100

    WIP on sort by file type (almost done)

--- srcs/sort_argv.c
+++ srcs/sort_argv.c
@@ -6,7 +6,7 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/24 14:59:31 by jguthert          #+#    #+#             */
-/*   Updated: 2016/02/24 23:39:01 by jguthert         ###   ########.fr       */
+/*   Updated: 2016/02/25 00:00:18 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,26 @@
 static void	link_to_fakefile(t_list **list, t_list *link)
 {
 	t_list	*prev;
+	t_list	*tamp;
 
 	prev = *list;
+	if (((t_file *)prev->content)->is_dir == 0
+		&& ((t_file *)prev->content)->is_fil == 0)
+		//mettre au debut;
 	while (prev->next != link)
 		prev = prev->next;
 	prev->next = link->next;
 	prev = *list;
-	while (((t_file *)(prev->next)->content)->is_dir == 0
-		   && ((t_file *)(prev->next)->content)->is_fil == 0)
+	tamp = (*list)->next;
+	while (tamp != NULL)
+	{
+		if (((t_file *)tamp->content)->is_dir == 0
+			&& ((t_file *)tamp->content)->is_fil == 0)
+			break ;
+		tamp = tamp->next;
 		prev = prev->next;
-	link->next = prev->next;
+	}
+	link->next = tamp;
 	prev->next = link;
 }
 
@@ -52,9 +62,13 @@ int			sort_argv(t_list **list)
 	while (link != NULL)
 	{
 		if (((t_file *)link->content)->is_fil == 0 && ((t_file *)link->content)->is_dir == 0)
+		{
+			printf("fakefile: [%s]\n", ((t_file *)link->content)->name);
 			link_to_fakefile(list, link);
+		}
 		else if (((t_file *)link->content)->is_dir == 1)
 			link_to_back(list, link);
+		link = link->next;
 	}
 	return (0);
 }
