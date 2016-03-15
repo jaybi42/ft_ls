66540624951ef79bdf4b9eddd7a7bf55064894c5
Jean-baptiste GUTHERTZ <jguthert@e3r3p14.42.fr>
2016-03-15 15:25:44 +0100

    added print_error

---
+++ srcs/print_ls_error.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_ls_error.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/14 15:33:03 by jguthert          #+#    #+#             */
+/*   Updated: 2016/03/14 15:33:45 by jguthert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+#include <string.h>
+
+void	print_error(char *name, int error)
+{
+	if (name == NULL)
+		return ;
+	ft_putstr("ls: ");
+	ft_putstr(name);
+	ft_putstr(": ");
+	ft_putendl(strerror(error));
+}
